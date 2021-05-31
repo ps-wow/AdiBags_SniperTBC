75bafd955d495729a6bb8e6b30c386723ed733ff
davidcraig <davidcraig1986@gmail.com>
2021-05-31 12:02:33 +0100

    Add some rings and neck from jewelcrafting

--- categories/boe/armour/jewelry.lua
+++ categories/boe/armour/jewelry.lua
@@ -2,4 +2,8 @@ local AddonName, AddonTable = ...
 
 AddonTable.boe.jewelry = {
   -- Rings
+  [20906] = true, -- Braided Copper Ring [Jewelcrafting]
+  [25439] = true, -- Tigerseye Band [Jewelcrafting]
+  -- Necks
+  [25438] = true, -- Malachite Pendant [Jewelcrafting]
 }
